81af7006d67be70a4d67aa9facd49380b1ad2676
dev-tch <tijeni.chaabeni@gmail.com>
2023-08-11 00:44:21 +0200

    string H

--- strings.h
+++ strings.h
@@ -1,5 +1,8 @@
 #ifndef STRINGS_H
 #define STRINGS_H
+char *_strdup(const char *str);
 int _str_len(char *str);
-char *_strdup(const char *source);
+char *_strcat(char *dest, char *src);
+int _strcmp(char *s1, char *s2);
+char *_strcpy(char *dest, char *src);
 #endif
