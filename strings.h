3317b72494049696b9c1de7f0208a3e6d7482135
dev-tch <tijeni.chaabeni@gmail.com>
2023-08-13 17:19:16 +0200

    add stchr

--- strings.h
+++ strings.h
@@ -7,5 +7,6 @@ int _strcmp(char *s1, char *s2);
 char *_strcpy(char *dest, char *src);
 int is_space(char c);
 int is_empty(char  *str);
-char *split(char *str, char separator);
+char *_strchr(char *s, char c);
+char *split(char *str, char *separators);
 #endif
