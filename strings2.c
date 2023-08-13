04707800a36594a1c37f86e70f025fb26324c134
dev-tch <tijeni.chaabeni@gmail.com>
2023-08-13 17:26:01 +0200

    add split

--- strings2.c
+++ strings2.c
@@ -1,9 +1,11 @@
 #include <stddef.h>
+#include <string.h>
 /**
 * is_space - check character space
 * @c: character
 * Return: (1 or 0)
 */
+
 int is_space(char c)
 {
 return (c == ' ' || c == '\t' || c == '\r');
@@ -13,6 +15,7 @@ return (c == ' ' || c == '\t' || c == '\r');
 * @str: sring
 * Return: (1 or 0)
 */
+
 int is_empty(char  *str)
 {
 	if (str == NULL)
@@ -31,30 +34,50 @@ int is_empty(char  *str)
 }
 
 /**
+ * _strchr - Entry point
+ * @s: input
+ * @c: input
+ * Return: Always 0 (Success)
+ */
+
+char *_strchr(char *s, char c)
+{
+	int i = 0;
+
+	for (; s[i] >= '\0'; i++)
+	{
+		if (s[i] == c)
+			return (&s[i]);
+	}
+	return (0);
+}
+
+/**
  * split - split a string based on a separator
  * @str: input string
  * @separator: separator character
  *
  * Return: pointer to the first token, NULL if no more tokens found
  */
-char *split(char *str, char separator)
+
+char *split(char *str, char *separators)
 {
 	static char *token;
 	char *result;
 
 	if (str != NULL)
 	{
-	token = str;
+		token = str;
 	}
 	if (token == NULL || *token == '\0')
 	{
-	return (NULL);
+		return (NULL);
 	}
 
 	result = token;
 	while (*token != '\0')
 	{
-		if (*token == separator)
+		if (strchr(separators, *token) != NULL)
 		{
 			*token = '\0';
 			token++;
