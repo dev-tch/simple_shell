19c02c7cee9d1ad4223d94aebdae99f1fc65cbdd
dev-tch <tijeni.chaabeni@gmail.com>
2023-08-23 01:21:32 +0000

    ommit code  from change directory

--- change_directory_builtin.c
+++ change_directory_builtin.c
@@ -17,40 +17,15 @@
 int change_dir(char *prg, int la, char **args, char **env,
 LinkedList **env_l, LinkedList **alia_l)
 {
-	char *new_dir;
-	char old_dir[256];
+	/*char *new_dir;*/
+	/*char old_dir[256];*/
 	/*unused parameters*/
 	(void)prg;
 	(void) env;
 	(void)env_l;
 	(void)alia_l;
-	if (la == 1)
-		new_dir = getenv("HOME"); /*No argument provided, change to $HOME*/
-	else if (strcmp(args[1], "-") == 0)
-		new_dir = getenv("OLDPWD");/*Handle cd - */
-	else
-		new_dir = args[1];
-	if (getcwd(old_dir, sizeof(old_dir)) == NULL)
-	{
-		perror("getcwd");
-		return (-1);
-	}
+	(void) args;
+	la = (la == 0) ? 0 : la;
 
-	if (chdir(new_dir) != 0)
-	{
-		perror("chdir");
-		return (-1);
-	}
-
-	if (setenv("OLDPWD", old_dir, 1) != 0)
-	{
-		perror("setenv");
-		return (-1);
-	}
-	if (setenv("PWD", new_dir, 1) != 0)
-	{
-		perror("setenv");
-		return (-1);
-	}
 	return (1);
 }
