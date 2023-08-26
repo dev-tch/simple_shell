7ec6bf195c2112e4cf660f3d888756b9119ca6cb
dev-tch <tijeni.chaabeni@gmail.com>
2023-08-26 18:13:25 +0000

    implement change directory

--- change_directory_builtin.c
+++ change_directory_builtin.c
@@ -1,6 +1,11 @@
 #include <stdlib.h>
 #include <unistd.h>
 #include "new_types.h"
+#include "common.h"
+#include "strings.h"
+
+char *get_value(char *var_name, LinkedList **env_t);
+int update_value(char *var_name, char *var_value, LinkedList **env_l);
 
 /**
  * Change_dir - change the current directory.
@@ -15,15 +20,105 @@
 int change_dir(char *prg, int la, char **args, char **env,
 LinkedList **env_l, LinkedList **alia_l)
 {
+	char *home, *pwd, *oldpwd;
+	char dest[256];
+
+	/*char cwd[256];*/
+	int ret = 0;
 	/*char *new_dir;*/
 	/*char old_dir[256];*/
 	/*unused parameters*/
 	(void)prg;
 	(void) env;
-	(void)env_l;
 	(void)alia_l;
-	(void) args;
-	la = (la == 0) ? 0 : la;
-
+	/*recuperation de PWD*/
+	/*recuperation de HOME*/
+	/*la ==1*/
+	/*la > 1*/
+	home = get_value("HOME", env_l);
+	pwd  = get_value("PWD", env_l);
+	oldpwd = get_value("OLDPWD", env_l);
+	if (la == 1)
+		_strcpy(dest, home);
+	else if (la > 1)
+	{
+		if (_strcmp(args[1], "-") == 0)
+			_strcpy(dest, oldpwd);
+		else
+		 _strcpy(dest, args[1]);
+	}
+	ret = chdir(dest);
+	if (ret == 0) /*on success*/
+	{
+		/*update_value("*/
+		update_value("OLDPWD", pwd, env_l);
+		update_value("PWD", dest, env_l);
+		/*update pwd*/
+	}
 	return (1);
+}
+
+/**
+* get_value - search env variable and return its value
+* @var_name: environnement variable name
+* @env_l: list contains environnments
+* Return: pointer to node in env_l
+*/
+char *get_value(char *var_name, LinkedList **env_l)
+{
+	LinkedList *tmp = *env_l;
+	int var_ok = 0, len = 0;
+	char *var_value  = NULL;
+
+	len = _strlen(var_name);
+	while (tmp != NULL)
+	{
+		var_ok = get_beginsWith(tmp->arg, var_name);
+		if (var_ok)
+		{
+			var_ok = 1;
+			var_value = (tmp->arg + len + 1);
+			break;
+		}
+		tmp = tmp->next;
+	}
+	return (var_value);
+}
+/**
+* update_value - assign new value to environnement variable
+* @var_name: environnement variable name
+* @var_value: new value used for update
+* @env_l: list of envirronnements
+* Return: (1 if update ok) else (0)
+*/
+int update_value(char *var_name, char *var_value, LinkedList **env_l)
+{
+	LinkedList *tmp = NULL;
+	int var_ok = 0;
+	char *old_data = NULL;
+	char *dest = NULL;
+
+	tmp = *env_l;
+	while (tmp != NULL)
+	{
+		var_ok = get_beginsWith(tmp->arg, var_name);
+		if (var_ok)
+		{
+			/*if variable is alreday exist ==>  update*/
+			dest = (char *) malloc(_strlen(var_name) + 1 + _strlen(var_value) + 1);
+			_strcpy(dest, var_name);
+			_strcat(dest, "=");
+			_strcat(dest, var_value);
+			old_data = tmp->arg;
+			tmp->arg  = _strdup(dest);
+			tmp->len  = _strlen(dest);
+			free(dest);
+			dest = NULL;
+			free(old_data);
+			old_data = NULL;
+			return (1);
+		}
+		tmp = tmp->next;
+	}
+	return (0);
 }
